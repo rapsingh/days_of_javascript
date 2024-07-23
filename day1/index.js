df4c9f8534e04bb97d8daeb3ae8d005b66ece69c
rapsingh <67787691+rapsingh@users.noreply.github.com>
2024-07-23 22:59:28 +0530

    typeOf

--- day1/index.js
+++ day1/index.js
@@ -1,5 +1,26 @@
 var oneVar = 10;
 console.log(oneVar);
-var myString="namaste";
+var myString="namaste javascript";
 console.log(myString);
+const myBool = true;
+console.log(myBool);
 
+
+let no =1;
+console.log(typeof(no));
+
+let sr = "ar"
+console.log(typeof(sr));
+
+let myBol = false;
+console.log(typeof(myBol));
+
+let myArr = ['qw','qt','qw'];
+console.log(typeof(myArr));
+
+let myObj = {
+    name:'Pratap',
+    age:21,
+    city:'Bengaluru'
+};
+console.log(typeof(myObj));
